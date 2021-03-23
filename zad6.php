c0e29fbc436372c68237e60a237ae71a0e2a95cd
Kessho212 <55281181+Kessho212@users.noreply.github.com>
2021-03-23 18:58:56 +0100

    Add files via upload

---
+++ zad6.php
@@ -0,0 +1,27 @@
+<?php
+/*Let us consider this example (array written in general format):
+
+ls = [0, 1, 3, 6, 10]
+
+Its following parts:
+
+ls = [0, 1, 3, 6, 10]
+ls = [1, 3, 6, 10]
+ls = [3, 6, 10]
+ls = [6, 10]
+ls = [10]
+ls = []
+The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
+The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.*/
+
+function partsSums($ls) {
+    $max = count($ls);
+    for($i=0;$i<=$max;$i++){
+      $tab[$i] = 0;
+      for($j=$i;$j<$max;$j++){
+        $tab[$i]+=$ls[$j];
+      }
+    }
+    return $tab;
+  }
